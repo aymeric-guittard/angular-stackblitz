8a816407b64510e203a432e1e0c23b4b46e6dfba
aymeric-guittard <33025315+aymeric-guittard@users.noreply.github.com>
2021-06-08 10:14:46 +0200

    ADDED Routing examples (#2)
    * ADDED basic routing : home page with redirection, and page not found
    
    * ADDED nested routing : a parent component with two children
    
    * ADDED catching incorrect paths on the nested path
    
    * ADDED Lazy Loaded Module
    
    * ADDED links to switch pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HomeComponent } from './pages/home/home.component';
+import { FirstChildComponent } from './pages/nested-pages/children/first-child/first-child.component';
+import { SecondChildComponent } from './pages/nested-pages/children/second-child/second-child.component';
+import { ParentComponent } from './pages/nested-pages/parent/parent.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: 'home'
+  },
+  {
+    path: 'nested',
+    component: ParentComponent, // this component should have a router-outlet in its template
+    children: [
+      {
+        path: 'first', // child route path
+        component: FirstChildComponent // child route component that the router renders
+      },
+      {
+        path: 'second',
+        component: SecondChildComponent // another child route component that the router renders
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'first'
+      },
+      {
+        component: NotFoundComponent,
+        path: '**' // if we don't catch these, the path "nested/doesntexist" will not display the parent component at all
+      }
+    ]
+  },
+  /*
+   * LAZY LOADED MODULE
+   */
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./pages/lazy-loaded/products/products.module').then(
+        m => m.ProductsModule
+      )
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    component: NotFoundComponent,
+    path: '**'
+  }
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ParentComponent } from './pages/nested-pages/parent/parent.component';
+import { FirstChildComponent } from './pages/nested-pages/children/first-child/first-child.component';
+import { SecondChildComponent } from './pages/nested-pages/children/second-child/second-child.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ParentComponent,
+    FirstChildComponent,
+    SecondChildComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
